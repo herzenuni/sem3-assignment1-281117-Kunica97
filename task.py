6f860b8b317e661b465506e65304a5c8326264f3
Sofiya Kunitsyna <31761683+Kunica97@users.noreply.github.com>
2017-12-04 13:34:09 +0300

    Add files via upload

---
+++ task.py
@@ -0,0 +1,14 @@
+# Куницына София ИВТ-2
+# задание 1
+
+def plus(ch):
+  sum = 0
+  sd = str(ch)
+  l = len(sd)
+  for i in range(l):
+    sum += int(sd[i])
+  return sum
+
+if __name__ == "__main__":
+  ch = int(input('Введите число:'))
+  print('Сумма чисел равна = {}'.format(plus(ch)))
